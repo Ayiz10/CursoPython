1acb9243f2ff53d9d60178487d84796473ee7c12
Ayiz <fuenteszarateayizdeth@gmail.com>
2020-11-03 12:31:56 -0600

    Agregue un espacio en el mensaje

--- conversor.py
+++ conversor.py
@@ -4,4 +4,4 @@ valor_dolar = 21.20
 dolares = pesos / valor_dolar
 dolares = round(dolares,2)
 dolares = str(dolares)
-print("Tienes $"+ dolares + "dolares")
+print("Tienes $"+ dolares + " dolares")
